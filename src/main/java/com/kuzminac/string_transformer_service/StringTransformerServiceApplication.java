dd073995fffc1ab432925d6858c85bd9485d4da8
Kukilej <marko19933@live.com>
2024-10-24 11:29:56 +0200

    Initial commit from Spring Initializer

---
+++ src/main/java/com/kuzminac/string_transformer_service/StringTransformerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kuzminac.string_transformer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringTransformerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringTransformerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuzminac/string_transformer_service/StringTransformerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzminac.string_transformer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringTransformerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
